d1fb41bee0fd7e42f5b4cee27e5e73d2c54ccc2e
karthik <karthikmudhigonda1@gmail.com>
2023-12-21 11:15:26 +0530

    Update userInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import Header from "./components/Header.jsx"
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue, //"+" forces to change string to number value in bts
+      };
+    });
+  }
+
+  const inputIsValid =
+    userInput.duration >= 1 && userInput.initialInvestment >= 1;
+
   return (
-    <Header/>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">
+          Please enter a duration or initial Investment greater than zero.
+        </p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
 }
 
-export default App
+export default App;
